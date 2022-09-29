6191734f9a8977dc529928e333639317f52dcb6f
JasperVR-1954275 <jasper.vanravenstein@student.uhasselt.be>
2022-09-29 10:09:19 +0200

    Mastermind

---
+++ Mastermind/src/Mastermind/MasterStrategy.java
@@ -0,0 +1,13 @@
+package Mastermind;
+
+
+/**
+ * This interface defines the method for the different computer strategies
+ *
+ *
+ */
+public interface MasterStrategy {
+   /* Make a single move as maker or continues moves as breaker */
+
+    
+}
---
+++ Mastermind/src/Mastermind/Mastermind.java
@@ -0,0 +1,30 @@
+package Mastermind;
+
+
+/**
+ * This class presents the game logic.
+ *
+ *
+ */
+public class Mastermind {
+
+    private static Mastermind $main;
+    private int attempts = 0;
+    private vector
+
+
+
+    private MasterStrategy $MasterStrategy;
+
+    /**
+     * The main method -- the program starts here
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+
+        } catch(Exception e) {
+            //code om alle onverwachte exceptions op te vangen en elegant af te handelen
+        }
+    }
+}
